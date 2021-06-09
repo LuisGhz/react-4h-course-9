de31ed6a433069aea36468986aa9efa0b8e660aa
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-08 22:10:55 -0500

    Import useUserCounter. Create buttons to increment, decrement and reset counter

--- src/Components/ComponentB.js
+++ src/Components/ComponentB.js
@@ -1,6 +1,16 @@
+import useUserCounter from 'customhooks/useUserCounter';
 
 const ComponentB = () => {
+  const [counter, increment, decrement, reset] = useUserCounter();
 
+  return (
+    <div>
+      <p>Counter B: { counter }</p>
+      <button onClick={ increment } >Increment</button>
+      <button onClick={ decrement } >Decrement</button>
+      <button onClick={ reset } >Reset</button>
+    </div>
+  );
 }
 
 export default ComponentB;
--- src/Components/ComponentC.js
+++ src/Components/ComponentC.js
@@ -1,6 +1,16 @@
+import useUserCounter from 'customhooks/useUserCounter';
 
 const ComponentC = () => {
+  const [counter, increment, decrement, reset] = useUserCounter();
 
+  return (
+    <div>
+      <p>Counter A: { counter }</p>
+      <button onClick={ increment } >Increment</button>
+      <button onClick={ decrement } >Decrement</button>
+      <button onClick={ reset } >Reset</button>
+    </div>
+  );
 }
 
 export default ComponentC;
