26ea0132600a6e5d3f2b3ecabdf31e3928a4f0df
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-08 22:03:47 -0500

    Modify counter. Create increment, decrement and reset functions modify counter

--- src/customhooks/useUserCounter.js
+++ src/customhooks/useUserCounter.js
@@ -1,6 +1,21 @@
+import { useState, useCallback } from 'react';
 
-const useUserCounter = (initialValue = 0, incrementer = 1) => {
+const useUserCounter = (initialValue = 0, modifier = 1) => {
+  const [counter, setCounter] = useState(initialValue);
 
+  const increment = useCallback(() => {
+    setCounter(counter + modifier);
+  }, [counter, modifier]);
+
+  const decrement = useCallback(() => {
+    setCounter(counter - modifier)
+  }, [counter, modifier]);
+
+  const reset = useCallback(() => {
+    setCounter(initialValue);
+  }, [initialValue]);
+
+  return [counter, increment, decrement, reset];
 }
 
 export default useUserCounter;
